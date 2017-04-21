c8b6b760c8f2ee68a1e2e75f53071934f98edef4
Gabriel Souto <gabrielaraujotigre@hotmail.com>
2017-04-21 16:46:21 -0300

    PagSeguroV2 com Atualizações
    Na segunda versão [V2] do sistema do PagSeguro foram feitas as seguintes mudanças:
    
    - Redirecionamento da página de pagamento para link na web
    - Possibilidade de mudança da quantidade de itens no carrinho
    - Verificação do funcionamento da API por meio de testes de notificação enviadas por email e pelo histórico de transação (ver fotos dos testes no Asana)
    
    By: Gabriel Souto

---
+++ PagseguroV2/README.txt.txt
@@ -0,0 +1,7 @@
+Na segunda versão [V2] do sistema do PagSeguro foram feitas as seguintes mudanças:
+
+- Redirecionamento da página de pagamento para link na web
+- Possibilidade de mudança da quantidade de itens no carrinho
+- Verificação do funcionamento da API por meio de testes de notificação enviadas por email e pelo histórico de transação (ver fotos dos testes no Asana)
+
+By: Gabriel Souto
---
+++ PagseguroV2/pagseguro.php
@@ -0,0 +1,32 @@
+<?php
+
+$data['token'] ='53C97A1C23DD48BB82DD3B1A85C585A4';
+$data['email'] = 'ruonlineunb@gmail.com';
+$data['currency'] = 'BRL';
+$data['itemId1'] = '1';
+$data['itemQuantity1'] = '1';
+$data['itemDescription1'] = 'Refeição do RU';
+$data['itemAmount1'] = '2.50';
+
+$url = 'https://ws.pagseguro.uol.com.br/v2/checkout';
+
+$data = http_build_query($data);
+
+$curl = curl_init($url);
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+$xml= curl_exec($curl);
+
+curl_close($curl);
+
+$xml= simplexml_load_string($xml);
+echo $xml -> code;
+
+
+?>
+
+ 
