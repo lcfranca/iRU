ffceeda9d862285bdd194a07437ec59e3f9a8f47
LucasBamidele <bamidele.lucas@gmail.com>
2017-05-05 00:51:50 -0300

    começado extrato e saldo em php

--- Aplicativo_LUCAS_ANDRE/DbController.php
+++ Aplicativo_LUCAS_ANDRE/DbController.php
@@ -5,28 +5,22 @@ Class Controller {
      $myConnect = new ConnectDB();
      $myConnect->Connect();
      $conn = $myConnect->conn;
-      if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
       $sql = "SELECT ID FROM Users WHERE Matricula = '$matricula' and Password = '$senha'";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       //$active = $row["active"];
-
       $count = mysqli_num_rows($result);
-
       // If result matched $myusername and $mypassword, table row must be 1 row
       if($count == 1) {
-         return true;
- //        session_register("$matricula");
- //        $_SESSION['login_user'] = $matricula;
-   //      header("location: localhost/Testinhos/dump.html");
+          $_SESSION['login_user'] = $matricula;
+          return true;
       }
-      else
-      {
-  //       $error = "Your Login Name or Password is invalid";
+      else {
          return false;
       }
-  //    return false;
+      //    return false;
    }
    }
 
@@ -36,11 +30,44 @@ Class Controller {
 		$conn = $myConnect->conn;
 		$criptografia = hash('sha256',$password);
 		//$criptografiaprabotapafuder = hash('sha256',$criptografia); Se quiser usar essa outra função,seria a criptografia da criptografia.
-		$sql = "INSERT INTO Users (Username, Matricula, Email, Password, CPF, Gender, Age, Curso) VALUES ('$username', '$matricula', '$email', '$criptografia', '$CPF', '$gender', '$age', '$curso')";
+		$sql = "INSERT INTO Users (Username, Matricula, Email, Password, CPF, Gender, Age, Curso, Saldo) VALUES ('$username', '$matricula', '$email', '$criptografia', '$CPF', '$gender', '$age', '$curso', '0')";
 		if(mysqli_query($conn, $sql)){
 			echo "Records inserted successfully.";
 		} else{
 			echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 		}
 	}
+  function checkUserCredit($matricula){
+    $myConnect = new ConnectDB();
+    $myConnect->Connect();
+    $conn = $myConnect->conn;
+    $sql = "SELECT SALDO FROM Users WHERE Matricula = '$matricula'";
+      if($result = mysqli_query($conn,$sql)) {
+          $row = mysqli_fetch_assoc($result);
+          echo $row["SALDO"];
+      }
+      else {
+         echo false;
+      }
+  }
+  function checkExtract($matricula){
+    $myConnect = new ConnectDB();
+    $myConnect->Connect();
+    $conn = $myConnect->conn;
+    $sql = "SELECT * FROM Compras WHERE Matricula = '$matricula'";
+    if($result = mysqli_query($conn,$sql)) {
+
+      /* criar objeto que tem 2 arrays (tempo e valor da compra (sera que rola um mapa?) e mandar via json encode) */
+          //tem como passar tudo no encode ou tem que fazer assim?
+          while($row = mysqli_fetch_assoc($result)){
+           // $buyinfo->date[0] = $
+          }
+      }
+      else {
+         echo false;
+      }
+  }
+
+
+
 }
